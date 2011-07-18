77a83090017e20695922bd0e2713e0bd0178810b
Joshua Bonnett <jbonnett@baycitizen.org>
2011-07-18 15:55:51 -0700

    now uses the test_suite

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,5 @@
 import unittest
-from fabric.decorators import *
+from armstrong.dev.tasks import *
 from armstrong.apps.audio.backends.id3reader.tests import Id3ReaderTestCase 
 
-@task 
-def test():
-    suite = unittest.TestLoader().loadTestsFromTestCase(Id3ReaderTestCase)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+test_suite = unittest.TestLoader().loadTestsFromTestCase(Id3ReaderTestCase)
